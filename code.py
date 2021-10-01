56664f565c76bf63004366277d7803f3fab56635
99061142 <99061142@mydavinci.nl>
2021-10-01 10:50:45 +0200

    input en if-statement

---
+++ code.py
@@ -0,0 +1,28 @@
+number_1_choosing = True
+number_2_choosing = True
+
+while number_1_choosing:
+    a = input("Kies het eerste getal: ")
+
+    try:
+        if int(a):
+            number_1_choosing = False
+    
+    except:
+        print("Kies een geldig getal (1, 2, 3, etc...")
+
+
+while number_2_choosing:
+    b = input("Kies het tweede getal: ")
+
+    try:
+        if int(b):
+            number_2_choosing = False
+    
+    except:
+        print("Kies een geldig getal (1, 2, 3, etc...")
+
+
+if a > b:
+    Max = a
+    print("a is het grootste getal: " + Max)
